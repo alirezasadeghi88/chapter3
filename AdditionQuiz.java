e4115b4a30ee92b3b3dfef5f9a9473872480f454
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-10-06 11:55:17 -0700

    R

--- AdditionQuiz.java
+++ AdditionQuiz.java
@@ -4,17 +4,17 @@ public class AdditionQuiz {
 	public static void main(String[] args) {
 		int number1 = (int)(System.currentTimeMillis() % 10);
 		int number2 = (int)(System.currentTimeMillis() / 10 % 10);
-
- 
+		int number3 = (int)(System.currentTimeMillis() % 10);
+		int number4 = (int)(System.currentTimeMillis() / 10 %10);
 		Scanner input = new Scanner(System.in);
 
 		System.out.print(
-		"What is " + number1 + " + " + number2 + "? ");
+		"What is " + number1 + " + " + number2 + " + " + number3 + " + " + number4 + " ? " );
 
 		int answer = input.nextInt();
 
 		System.out.println(
-		number1 + " + " + number2 + " = " + answer + " is " +
-		(number1 + number2 == answer));
+		number1 + " + " + number2 + " + " + number3 + " + " + number4 + " = " + answer + " is " +
+		(number1 + number2 + number3 + number4 == answer));
 	}
  }
