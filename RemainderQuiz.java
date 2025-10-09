c18701cb6cb8fccabce81c07d8ad74e595ee466c
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-10-09 06:01:16 -0700

    R

---
+++ RemainderQuiz.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class RemainderQuiz {
+	public static void main(String [] args) {
+		
+		int number1 = (int)(Math.random() * 1000 );
+		int number2 = (int)(Math.random() * 1000 );
+		
+		
+		if (number1 < number2) {
+			int temp = number1;
+			number1 = number2;
+			temp = number2;
+		}
+		
+		
+		System.out.print
+		("What is" + number1 + " % " + number2 + " ? ");
+		Scanner input = new Scanner(System.in);
+		int answer = input.nextInt();
+		
+		
+		if (number1 % number2 == answer)
+			System.out.println("You are correct!");
+		else {
+			System.out.println("Your answer is wrong. ");
+			System.out.println(number1 + " % " + number2 +
+			"should be" + (number1 % number2));
+		}
+	}
+}
