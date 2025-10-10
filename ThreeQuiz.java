00f8082c64042f06431f42d186a754d0ac3d686a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-10-10 01:29:50 -0700

    R

---
+++ ThreeQuiz.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ThreeQuiz {
+	public static void main(String[] args) {
+	
+	
+	int numbre1 = (int)(Math.random() * 100 );
+	int numbre2 = (int)(Math.random() * 100 );
+	int numbre3 = (int)(Math.random() * 100 );
+	
+	if (numbre1 < numbre2 ) {
+		int temp = number1;
+		number1 = number2;
+		temp = number2;
+		
+	}	
+	if (numbre1 < numbre3) {
+		int temp = number1;
+		number1 = number3;
+		temp = number3;
+	
+	}
+	
+	System.out.print
+		("What is" + number1 + " + " + number2 + " + " + numbre3 + " ? ");
+		Scanner input = new Scanner(System.in);
+		int answer = input.nextInt();
+		
+	if (number1 + number2 == answer)
+		System.out.println("You are correct!");
+		
+	if (number1 + number3 == answer)
+		System.out.println("You are correct!");
+	
+    else {
+	    System.out.println("Your answer is wrong. ");
+		System.out.println(number1 + " + " + number2 
+			"should be" + (number1 + number2 ));
+	}	
+ 	 else {
+	    System.out.println("Your answer is wrong. ");
+		System.out.println(number1 + " + " + number3 
+		"should be" + (number1 + number3 ));
+	}	
+	}
+}
